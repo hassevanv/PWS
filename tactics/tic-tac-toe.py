48409a9c9ad2ed5ca32afb1262fdd394283fd3b5
default <hassevanvliet@gmail.com>
2026-06-29 10:54:09 +0000

    Tactiek toevoegen

--- tactics/tic-tac-toe.py
+++ tactics/tic-tac-toe.py
@@ -1,5 +1,38 @@
 import random
 
 def choose_move(board, big_board, forced_section):
+
+    SMALL_CORNERS = (1, 3, 7, 9)
+    BIG_CORNERS = (10, 30, 70, 90)
+
+    if forced_section is None: # als er vrije keuze is
+        available_big_corners = [big for big in BIG_CORNERS # filter vrije grote hoeken
+                                 if big_board[big] == 0 and not all(board[big + s] != 0 for s in range(1, 10))]
+        
+        if available_big_corners:
+            chosen_section = random.choice(available_big_corners) # random grote hoek kiezen uit de selectie
+        else:
+            available_sections = [big for big in (10, 20, 30, 40, 50, 60, 70, 80, 90) # vaststellen welke niet-hoeken er nog beschikbaar zijn
+                                  if big_board[big] == 0 and not all(board[big + s] != 0 for s in range(1, 10))]
+            chosen_section = random.choice(available_sections) # random sectie kiezen uit de beschikbare secties
+    else:
+        chosen_section = forced_section # naar de forced_section gaan als er een is
+
+    available_cells_corners = [s for s in SMALL_CORNERS if board[chosen_section + s] == 0] # vrije lege kleine hoeken vaststellen
+    player_cells_corners = [s for s in SMALL_CORNERS if board[chosen_section + s] == current_player]
+    surrounding_corners = ((3,7),(1,3),(1,9),(1,7),(0,0),(3,9),(1,9),(7,9),(3,7)) # Lijst met omliggende hoeken ingedeeld als [0] is vakje 1 [1] is vakje 2 [2] is vakje 3 etc. (vakje 5 heeft gen surrounding corners)
+
+    if player_cells_corners:
+        # Als de speler een hoekje heeft kies dan een hoekje ernaast (met de laagste index)
+        # if available_cells_corners in surrounding_corners[i-1] for i in player_cells_corners:
+        if surrounding_corners[i-1][0] in available_cells_corners for i in player_cells_corners:
+            chosen_cell = surrounding_corners[i-1][0]
+        elif surrounding_corners[i-1][1] in available_cells_corners for i in player_cells_corners:
+            chosen_cell = surrounding_corners[i-1][1]
+    elif available_cells_corners:
+        chosen_cell = random.choice(available_cells_corners) # random vrij hoekje kiezen
+    else:
+        available_cells = [s for s in range(1, 10) if board[chosen_section + s] == 0] # random vrij vakje keizen als er geen hoekjes zijn
+        chosen_cell = random.choice(available_cells)
     
-    return 0
+    return chosen_section, chosen_cell # return de gekozen sectie en het gekozen vakje
